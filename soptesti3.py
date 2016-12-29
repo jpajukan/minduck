771327e9d069a3991d6cd40636d2ba2d8b9fefb5
jussiii <jussi.kurikka@gmail.com>
2016-12-29 22:07:18 +0200

    Add files via upload

--- soptesti3.py
+++ soptesti3.py
@@ -1,10 +1,30 @@
-# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
 import numpy as np
- 
+
+def segmentation(arg,image_gray):
+        if arg == 1: #threshold segmentaatio
+                (thresh, image_bw) = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                return image_bw;
+        elif arg == 2: #watershed? segmentaatio floodfill?
+                print "testi"
+                                
+        return;
+
+def blur(arg,image_gray):
+        if arg == 1:#Averaging
+                image_gray = cv2.blur(image_gray,(5,5))
+        elif arg == 2:#Gaussian Blurring
+                image_gray = cv2.GaussianBlur(image_gray,(5,5),0)
+        elif arg == 3:#Median Blurring
+                image_gray = cv2.medianBlur(image_gray,5)
+        elif arg == 4:#Bilateral Filtering
+                image_gray = cv2.bilateralFilter(image_gray,9,75,75)
+        return;
+        
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -13,62 +33,66 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 redBoundLow = np.array([0,0,100], dtype="uint8")
 redBoundUp = np.array([50,56,255], dtype="uint8")
- 
-# allow the camera to warmup
-time.sleep(2)
- 
-# capture frames from the camera
+
+# viive kameraa varten
+time.sleep(0.1)
+
+# ota kuvia kamerasta
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
+	#tee NumPy taulukko kameran kuvasta
 	image = frame.array
 
-        #image_gray = cv2.imread(imfile,cv2.CV_LOAD_IMAGE_GRAYSCALE);
-
+        #muuta kuva mustavalkoiseksi
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #retval, dst = cv2.threshold(imageGray, 150, 255, cv2.THRESH_BINARY)
 
-        #img2rgb = cv2.cvtColor(dst, cv2.COLOR_GRAY2RGB)
+        #suodatus
+        #0 == ei mitaan
+        #1 == Averaging
+        #2 == Gaussian Blurring
+        #3 == Median Blurring
+        #4 == Bilateral Filtering
+        blur(3,image_gray);
 
-        (thresh, image_bw) = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        #segmentointi, jos parametri on:
+        #1 == threshold (otsu)
+        #2 == watershed?
+        image_bw = segmentation(1,image_gray);
+
+        #canny reunantunnistus
         edges = cv2.Canny(image_bw,100,200)
 
-        #houghLines = cv2.HoughLinesP(edges, 1, 3.14/180, 10, 100, minLineLength=50, maxLineGap=30)
-        houghLines = cv2.HoughLinesP(image=edges,rho=0.3,theta=np.pi/200, threshold=10,lines=np.array([]),minLineLength=50,maxLineGap=30)
+        houghLines = cv2.HoughLinesP(image=edges,rho=0.3,theta=np.pi/200, threshold=10,lines=np.array([]),minLineLength=50,maxLineGap=30)        
 
-        #print houghLines
-
-        #minLineLength = 30
-        #maxLineGap = 10
-        #lines = cv2.HoughLinesP(edges,1,np.pi/180,15,minLineLength,maxLineGap)
-        if len(houghLines) > 0:
+        if houghLines is not None:  # tarkista etta houghlines on olemassa
                 houghLines = houghLines[0]
-                #print len(houghLines)
-                #print houghLines
-                #for x in range(0, len(houghLines)):
-                for x1,y1,x2,y2 in houghLines:
+                for x1,y1,x2,y2 in houghLines: #piirra houghlinet kuvaan
                         cv2.line(image,(x1,y1),(x2,y2),(255,0,0),2)
-
+        # GrabCut
+##        mask = np.zeros(image.shape[:2],np.uint8)
+##        bgdModel = np.zeros((1,65),np.float64)
+##        fgdModel = np.zeros((1,65),np.float64)
+##        rect = (50,50,450,290)
+##        cv2.grabCut(image,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+##        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 
         #mask = cv2.inRange(image, redBoundLow, redBoundUp)
-
         #target = cv2.bitwise_and(image, image, mask = mask)
-        
-	
+       	
         #imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         #retval, dst = cv2.threshold(imageGray, 150, 255, cv2.THRESH_BINARY)
 
         #img2rgb = cv2.cvtColor(dst, cv2.COLOR_GRAY2RGB)
-        
-        
+                
         #output = img2rgb+target
-	# show the frame
+
+                                
+	# nayta kuva
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
  
-	# clear the stream in preparation for the next frame
+	# tyhjenna stream seuraavaa kuvaa varten
 	rawCapture.truncate(0)
  
-	# if the `q` key was pressed, break from the loop
+	# jos painetaan `q` nappia, niin ohjelma loppuu
 	if key == ord("q"):
 		break
